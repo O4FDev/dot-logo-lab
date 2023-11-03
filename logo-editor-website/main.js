c3d2539aabecb52bc536e2fd58891d5b11388690
O4FDev <luke.lucas@ou.ac.uk>
2023-11-03 11:59:18 +0000

    fix(project): injecting analytics

--- logo-editor-website/main.js
+++ logo-editor-website/main.js
@@ -1,3 +1,7 @@
 import { inject } from '@vercel/analytics'
+// import some helper that is exposed by your current framework to determine the right mode manually
+import { dev } from '$app/environment'
 
-inject();
+inject( {
+    mode: dev ? 'development' : 'production',
+} )
